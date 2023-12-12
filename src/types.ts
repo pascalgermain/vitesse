fc47dec9a5ecbcbb139a66843ced975eba32e1a6
Pascal Germain <pascalgermain@me.com>
2023-12-12 17:21:19 +0100

    example

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,9 @@
 import type { ViteSSGContext } from 'vite-ssg'
 
 export type UserModule = (ctx: ViteSSGContext) => void
+
+// This triggers error TS4081: Exported type alias 'NumberRef' has or is using private name 'Ref'.
+export type NumberRef = Ref<number>
+
+// This triggers error TS4025: Exported variable 'count' has or is using private name 'Ref'.
+export const count: Ref<number> = ref(1)
--- uno.config.ts
+++ uno.config.ts
@@ -1,4 +1,5 @@
 import {
+  type UserConfig,
   defineConfig,
   presetAttributify,
   presetIcons,
@@ -9,7 +10,10 @@ import {
   transformerVariantGroup,
 } from 'unocss'
 
-export default defineConfig({
+// Had to add this to prevent error TS2742: The inferred type of 'default' cannot be
+// named without a reference to '.pnpm/@unocss+core@0.58.0/node_modules/@unocss/core'.
+// This is likely not portable. A type annotation is necessary.
+export default <UserConfig>defineConfig({
   shortcuts: [
     ['btn', 'px-4 py-1 rounded inline-block bg-teal-700 text-white cursor-pointer !outline-none hover:bg-teal-800 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
     ['icon-btn', 'inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out hover:opacity-100 hover:text-teal-600'],
